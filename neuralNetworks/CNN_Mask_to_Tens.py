7ff16b273928bed69104de1ffd79b58bdc7c9c04
svanesch <90719713+svanesch@users.noreply.github.com>
2023-07-21 14:58:31 +0200

    Add files via upload

---
+++ neuralNetworks/CNN_Mask_to_Tens.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.conv4 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.conv5 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.fc1 = nn.Linear(256 * 2 * 2, 1024)
+        self.fc2 = nn.Linear(1024, 512)
+        self.fc3 = nn.Linear(512, 256)
+        self.fc4 = nn.Linear(256, 128)
+        self.fc5 = nn.Linear(128, 1)
+        self.leaky_relu = nn.LeakyReLU(negative_slope=0.2)
+
+    def forward(self, x):
+        x = self.pool(self.leaky_relu(self.conv1(x)))
+        x = self.pool(self.leaky_relu(self.conv2(x)))
+        x = self.pool(self.leaky_relu(self.conv3(x)))
+        x = self.pool(self.leaky_relu(self.conv4(x)))
+        x = self.pool(self.leaky_relu(self.conv5(x)))
+        x = x.view(-1, 256 * 2 * 2)
+        x = self.leaky_relu(self.fc1(x))
+        x = self.leaky_relu(self.fc2(x))
+        x = self.leaky_relu(self.fc3(x))
+        x = self.leaky_relu(self.fc4(x))
+        x = self.fc5(x)
+        return x
---
+++ neuralNetworks/UNetwork.py
@@ -0,0 +1,95 @@
+import torch
+import torch.nn as nn
+
+class UNet(nn.Module):
+    def __init__(self):
+        super(UNet, self).__init__()
+
+        # Encoder (downsampling path)
+        self.enc_conv1 = nn.Conv2d(3, 64, kernel_size=3, padding=1)
+        self.enc_conv2 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        self.enc_pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.enc_conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.enc_conv4 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
+        self.enc_pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.enc_conv5 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.enc_conv6 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        self.enc_pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.enc_conv7 = nn.Conv2d(256, 512, kernel_size=3, padding=1)
+        self.enc_conv8 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.enc_pool4 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # Bottleneck
+        self.bottleneck_conv1 = nn.Conv2d(512, 1024, kernel_size=3, padding=1)
+        self.bottleneck_conv2 = nn.Conv2d(1024, 1024, kernel_size=3, padding=1)
+
+        # Decoder (upsampling path)
+        self.dec_upsample1 = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False)
+        self.dec_conv1 = nn.Conv2d(1024 + 512, 512, kernel_size=3, padding=1)
+        self.dec_conv2 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+
+        self.dec_upsample2 = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False)
+        self.dec_conv3 = nn.Conv2d(512 + 256, 256, kernel_size=3, padding=1)
+        self.dec_conv4 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+
+        self.dec_upsample3 = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False)
+        self.dec_conv5 = nn.Conv2d(256 + 128, 128, kernel_size=3, padding=1)
+        self.dec_conv6 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
+
+        self.dec_upsample4 = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False)
+        self.dec_conv7 = nn.Conv2d(128 + 64, 64, kernel_size=3, padding=1)
+        self.dec_conv8 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+
+          # Output
+        self.output_conv = nn.Conv2d(64, 1, kernel_size=1)  # 1x1 convolution for binary segmentation
+
+
+    def forward(self, x):
+        # Encoder (downsampling path)
+        enc1 = nn.ReLU()(self.enc_conv1(x))
+        enc2 = nn.ReLU()(self.enc_conv2(enc1))
+        enc2_pool = self.enc_pool1(enc2)
+
+        enc3 = nn.ReLU()(self.enc_conv3(enc2_pool))
+        enc4 = nn.ReLU()(self.enc_conv4(enc3))
+        enc4_pool = self.enc_pool2(enc4)
+
+        enc5 = nn.ReLU()(self.enc_conv5(enc4_pool))
+        enc6 = nn.ReLU()(self.enc_conv6(enc5))
+        enc6_pool = self.enc_pool3(enc6)
+
+        enc7 = nn.ReLU()(self.enc_conv7(enc6_pool))
+        enc8 = nn.ReLU()(self.enc_conv8(enc7))
+        enc8_pool = self.enc_pool4(enc8)
+
+        # Bottleneck
+        bottleneck = nn.ReLU()(self.bottleneck_conv1(enc8_pool))
+        bottleneck = nn.ReLU()(self.bottleneck_conv2(bottleneck))
+
+        # Decoder (upsampling path)
+        dec1 = self.dec_upsample1(bottleneck)
+        dec1 = torch.cat([dec1, enc8], dim=1)
+        dec1 = nn.ReLU()(self.dec_conv1(dec1))
+        dec1 = nn.ReLU()(self.dec_conv2(dec1))
+
+        dec2 = self.dec_upsample2(dec1)
+        dec2 = torch.cat([dec2, enc6], dim=1)
+        dec2 = nn.ReLU()(self.dec_conv3(dec2))
+        dec2 = nn.ReLU()(self.dec_conv4(dec2))
+
+        dec3 = self.dec_upsample3(dec2)
+        dec3 = torch.cat([dec3, enc4], dim=1)
+        dec3 = nn.ReLU()(self.dec_conv5(dec3))
+        dec3 = nn.ReLU()(self.dec_conv6(dec3))
+
+        dec4 = self.dec_upsample4(dec3)
+        dec4 = torch.cat([dec4, enc2], dim=1)
+        dec4 = nn.ReLU()(self.dec_conv7(dec4))
+        dec4 = nn.ReLU()(self.dec_conv8(dec4))
+
+        # Output
+        output = self.output_conv(dec4)
+        return output   
